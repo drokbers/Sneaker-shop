a235c77b7d8d1748cbf0b4b0974058a96b281737
Serdar Aksoy <aksoyserdar7@gmail.com>
2023-06-15 18:01:20 -0400

    Reducers:  addToCart, removeFromCart, clearCart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,43 +1,75 @@
-import { useSelector, useDispatch } from 'react-redux';
-import React from 'react';
-import { removeFromCart } from '../../store/cartSlice';
+import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import Image from "next/image";
 
-
-
+import { clearCart } from "../../store/cartSlice";
 
 const Cart = () => {
-  const cartItems = useSelector((state) => state.cart);
-  const cartIsOpen = useSelector(state => state.toggle);
+  const cartItems = useSelector((state) => state.cart.items) || [];
+
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
+
   const dispatch = useDispatch();
 
-  const handleRemoveFromCart = itemId => {
-    dispatch(removeFromCart(itemId));
-  };
 
   const handleClearCart = () => {
     dispatch(clearCart());
   };
 
   return (
-    <div>
-    <h2>Cart</h2>
-    {cartItems.length === 0 ? (
-      <p>Your cart is empty.</p>
-    ) : (
-      <>
-        { cartIsOpen && cartItems.map(item => (
-          <div key={item.id}>
-            <h3>{item.title}</h3>
-            <p>Price: ${item.price}</p>
-            <p>Quantity: {item.quantity}</p>
-            <button onClick={() => handleRemoveFromCart(item.id)}>Remove from Cart</button>
+    <div className="flex absolute right-5 justify-end w-75 h-60 rounded-lg shadow-md p-4  ">
+      
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <div className="flex flex-col justify-between ">
+          <div
+            id="cart title"
+            className="font-sans  text-neutral-black font-bold border-b-2"
+          >
+            <h1>Cart</h1>
+            
           </div>
-        ))}
-        <button onClick={handleClearCart}>Clear Cart</button>
-      </>
-    )}
+
+          <div id='urun bilgi'className=" justify-start">
+          {cartItems.map((item) => (
+              <div key={item.id} className="flex gap-2 ">
+                <Image
+                  className="rounded-md justify-start "
+                  src={item.subImage1}
+                  width={50}
+                  height={50}
+                  alt="DENME"
+                  id="foto"
+                />
+                <div className="">
+                  <h3>{item.title}</h3>
+                  <p>
+                    ${item.price} x {item.quantity} {totalAmount.toFixed(2)}
+                  </p>
+                </div>
+                <button onClick={handleClearCart}>
+                <Image
+                  className="rounded-md justify-start "
+                  src={'/images/icon-delete.svg'}
+                  width={15}
+                  height={15}
+                  alt="DENME"
+                  id="foto"
+                />
+                </button>
+              </div>
+            ))}
+
+          </div>
+          <div> 
+          <button className="flex items-center  justify-center bg-primary-orange text-white px-4 py-2 rounded-md w-60 h-12">
+              <p>Checkout</p>
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-    
   );
 };
 
